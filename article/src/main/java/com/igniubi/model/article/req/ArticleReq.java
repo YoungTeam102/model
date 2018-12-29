46f26deb366a709c3885eab25afd1ee2655abef3
shuai.zhang <shuai.zhang@yoho.cn>
2018-12-29 10:54:52 +0800

    验参

--- article/src/main/java/com/igniubi/model/article/req/ArticleReq.java
+++ article/src/main/java/com/igniubi/model/article/req/ArticleReq.java
@@ -3,6 +3,7 @@ package com.igniubi.model.article.req;
 import com.igniubi.model.BaseModel;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Data
@@ -18,6 +19,7 @@ public class ArticleReq extends BaseModel {
 
     private String imgUrl;
 
+    @NotBlank(message = "日期不能为空")
     private String date;
 
 }
